4f255e58c59ea4fac523f454ef52a8fd8b5ea7e4
Scala Steward <me@scala-steward.org>
2021-02-26 12:55:49 +0100

    Update airstream to 0.12.0

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -2,7 +2,7 @@ object BuildSettings {
 
   object version {
 
-    val airstream      = "0.12.0-RC1"
+    val airstream      = "0.12.0"
     val `tuplez-apply` = "0.3.1"
     val utest          = "0.7.7"
 
