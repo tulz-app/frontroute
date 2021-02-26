8a2b8eb402a03324aeac1fbdda3d0861c8f309ce
Iurii Malchenko <i@yurique.com>
2021-02-26 18:38:42 +0200

    update tuplez to 0.3.4

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -3,7 +3,7 @@ object BuildSettings {
   object version {
 
     val airstream      = "0.12.0"
-    val `tuplez-apply` = "0.3.4-SNAPSHOT"
+    val `tuplez-apply` = "0.3.4"
     val utest          = "0.7.7"
     val munit          = "0.7.22"
 
