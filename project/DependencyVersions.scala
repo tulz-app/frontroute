855fab73b06ff2f83d200fdc3216151d46deef52
Iurii Malchenko <i@yurique.com>
2021-07-27 11:27:33 +0300

    website deps update

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -4,7 +4,7 @@ object DependencyVersions {
   val `tuplez-apply` = "0.3.6"
   val utest          = "0.7.10"
   //wesite
-  val laminext               = "0.13.9"
+  val laminext               = "0.13.10"
   val sourcecode             = "0.2.7"
   val `embedded-files-macro` = "0.2.4"
 
