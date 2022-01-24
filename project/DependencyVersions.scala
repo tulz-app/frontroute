d3e33584f98568543657e9ac43ba0aba0ee8bfc4
Scala Steward <me@scala-steward.org>
2022-01-24 05:12:09 +0100

    Update utest to 0.7.11

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -2,7 +2,7 @@ object DependencyVersions {
 
   val airstream      = "0.14.2"
   val `tuplez-apply` = "0.3.6"
-  val utest          = "0.7.10"
+  val utest          = "0.7.11"
 
   // website
   val laminext               = "0.14.0"
