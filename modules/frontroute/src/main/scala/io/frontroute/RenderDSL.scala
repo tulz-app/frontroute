7087c1a0d9d523ed6224f61bd7437bfcc19ded96
Iurii Malchenko <yurique@astraea.io>
2022-05-29 19:20:47 +0200

    signal of element to route

--- modules/frontroute/src/main/scala/io/frontroute/RenderDSL.scala
+++ modules/frontroute/src/main/scala/io/frontroute/RenderDSL.scala
@@ -4,6 +4,7 @@ import com.raquo.laminar.api.L._
 
 trait RenderDSL extends RouteDSL[Element] {
 
-  implicit def elementToRoute(e: Element): Route = complete(e)
+  implicit def elementToRoute(e: Element): Route                = complete(e)
+  implicit def signlOfElementToRoute(e: Signal[Element]): Route = complete(e)
 
 }
