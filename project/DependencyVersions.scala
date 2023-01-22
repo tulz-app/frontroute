0351eea1822596ecac5114c3b95231aa7daef92d
Iurii Malchenko <i@yurique.com>
2023-01-22 16:02:51 +0100

    deps

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -4,12 +4,12 @@ object DependencyVersions {
   val `tuplez-apply`              = "0.3.8"
   val utest                       = "0.8.0"
   val domtestutils                = "0.15.3"
-  val scalatest                   = "3.2.14"
+  val scalatest                   = "3.2.15"
   val jsdom                       = "16.4.0"
   val `scalajs-java-securerandom` = "1.0.0"
 
   // tests
-  val `scala-js-macrotask-executor` = "1.1.0"
+  val `scala-js-macrotask-executor` = "1.1.1"
 
   // website
   val laminext               = "0.14.0"
