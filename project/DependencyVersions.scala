b087b3a66600f4edbc393c749ad04f3b1f4d1f1e
Scala Steward <scala_steward@virtuslab.com>
2023-03-20 19:31:12 +0000

    Update domtestutils to 17.0.0

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -3,7 +3,7 @@ object DependencyVersions {
   val laminar                     = "0.14.5"
   val `tuplez-apply`              = "0.3.8"
   val utest                       = "0.8.0"
-  val domtestutils                = "0.15.3"
+  val domtestutils                = "17.0.0"
   val scalatest                   = "3.2.15"
   val jsdom                       = "16.4.0"
   val `scalajs-java-securerandom` = "1.0.0"
