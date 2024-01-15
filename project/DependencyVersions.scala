081c90862d40cbc26b9cdabc6f599245e9e4f24f
Iurii Malchenko <yurique@astraea.io>
2024-01-15 09:41:29 +0100

    Laminar 17.0.0-M6

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -1,8 +1,8 @@
 object DependencyVersions {
 
-  val laminar        = "17.0.0-M2"
+  val laminar        = "17.0.0-M6"
   val `tuplez-apply` = "0.4.0"
-  val domtestutils   = "17.0.0"
+  val domtestutils   = "18.0.1"
   val scalatest      = "3.2.17"
 
   // tests
