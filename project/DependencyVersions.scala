7ef96cb1275bf3d53a85c93a59f4d91cb4d8c3b6
Scala Steward <scala_steward@virtuslab.com>
2026-01-29 14:02:15 +0000

    Update domtestutils to 19.0.0

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -2,7 +2,7 @@ object DependencyVersions {
 
   val laminar        = "17.2.0"
   val `tuplez-apply` = "0.4.0"
-  val domtestutils   = "18.0.1"
+  val domtestutils   = "19.0.0"
   val scalatest      = "3.2.19"
 
   // tests
